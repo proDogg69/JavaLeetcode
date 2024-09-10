eadb5e9923f898be845836cf73c21a99be8297b0
proDogg69 <tyl3rh4rrison@gmail.com>
2024-09-10 15:23:32 +0630

    EDIT: Commented code

--- test.java
+++ test.java
@@ -12,29 +12,45 @@ public class test {
         values.put('C', 100);
         values.put('D', 500);
         values.put('M', 1000);
-        int before = 0;
-        int after = 0;
+        int dummy = 0;
+        int number = 0;
         int result = 0;
-        int counter = 0;
-        while (!(counter == s.length())) {
-            if (before == 0) {
-                before = values.get(s.charAt(counter));
-                after = values.get(s.charAt(counter));
-                result += after;
+        for (int i = 0; i < s.length(); i++) {
+            if (dummy == 0) {
+                dummy = values.get(s.charAt(i));
+                result += dummy;
             } else {
-                after = values.get(s.charAt(counter));
-                if (after - before == 4 || after - before == 9 ||
-                        after - before == 40 || after - before == 90 ||
-                        after - before == 400 || after - before == 900) {
-                    result -= before;
-                    result = after - before;
+                number = values.get(s.charAt(i));
+                if (number - dummy == 4 || number - dummy == 9 ||
+                        number - dummy == 40 || number - dummy == 90 ||
+                        number - dummy == 400 || number - dummy == 900) {
+                    result -= dummy;
+                    result += number - dummy;
+                    dummy = 0;
+                    System.out.println(result);
                 } else {
-                    result += after;
+                    result += number;
+                    dummy = number;
                 }
-                before = after;
             }
-            counter++;
         }
-        System.out.println(result);
     }
 }
+// BETTER SOLUTION FROM OTHER SUBMISSIONS
+// public int romanToInt(String s) {
+// int ans = 0, num = 0;
+// for (int i = s.length()-1; i >= 0; i--) {
+// switch(s.charAt(i)) {
+// case 'I': num = 1; break;
+// case 'V': num = 5; break;
+// case 'X': num = 10; break;
+// case 'L': num = 50; break;
+// case 'C': num = 100; break;
+// case 'D': num = 500; break;
+// case 'M': num = 1000; break;
+// }
+// if (4 * num < ans) ans -= num;
+// else ans += num;
+// }
+// return ans;
+// }
